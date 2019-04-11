308c875928fab2bad67ad537afd9105ef7ab13e3
Aswanth <aswanth@am.students.amrita.edu>
2019-04-11 13:38:20 +0530

    changes

--- app/forms.py
+++ app/forms.py
@@ -4,17 +4,17 @@ from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
-    fname = StringField('First Name', validators=[DataRequired()])
-    lname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    roll = StringField('Roll Number', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    fname = StringField('First Name', validators=[DataRequired()], render_kw={"placeholder": "First Name"})
+    lname = StringField('Last Name', validators=[DataRequired()], render_kw={"placeholder": "Last Name"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
+    roll = StringField('Roll Number', validators=[DataRequired()], render_kw={"placeholder": "Roll Number"})
+    password = PasswordField('Password', validators=[DataRequired()],render_kw={"placeholder": "Password"})
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Repeat Password"})
     submit = SubmitField('Register')
 
     def validate_roll(self, roll):
@@ -33,10 +33,10 @@ class RegistrationForm(FlaskForm):
 
 class AddForm(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
-	roll = StringField('Recipient Roll Number', validators=[DataRequired()])
+	roll = StringField('Recipient roll number', validators=[DataRequired()])
 	tracking_id = StringField('Tracking Id',validators=[DataRequired()])
 	submit = SubmitField('Add')
 	def validate_roll(self,roll):
 		user = User.query.filter_by(roll=roll.data.lower()).first()
 		if user is None:
-			raise ValidationError('Please use a valid Roll Number.')
+			raise ValidationError('Please use a valid roll number.')
